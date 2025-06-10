673e0a3fe9ce307fd974fbbfdaab7b0b35e0431f
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-06-10 15:53:47 +0500

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -144,7 +144,7 @@ export const fetchUserProfile = async (req, res) => {
   const userId = req.user.id;
 
   try {
-    const user = await prisma.user.findUnique(userId);
+    const user = await prisma.user.findUnique({where:{id: userId}});
 
     if (!user) {
       return res.status(404).json({ message: 'User not found' });

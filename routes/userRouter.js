8065022dcaca0e4f12ded89021f208bb230591b2
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-06-10 07:50:22 +0500

    update

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,13 +1,6 @@
 import express from 'express';
 const router = express.Router();
 
-import { loginUser, registerUser, fetchUserProfile, updateUserProfile } from "../controllers/authController.js";
-
-router.post('/registerUser', registerUser);
-router.post('/loginUser', loginUser);
-router.get('/fetchUserProfile/:id', fetchUserProfile);
-router.patch('/updateUserProfile/:usrId', updateUserProfile)
-
 import { loginUser, registerUser, fetchUserProfile, updateUserProfileDp,deleteAccount } from "../controllers/authController.js";
 import { authenticateUser } from '../middleware/authMiddleware.js';
 

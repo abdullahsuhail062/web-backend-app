a5649bb9fc8c557d089133f5153d9da5b7e70b93
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-06-10 17:35:50 +0500

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,24 +15,6 @@ import { validateRegisterUser } from '../validators/authValidator.js';
 
 const prisma = new PrismaClient();
 
-// ✅ Update Profile (General)
-export const updateUserProfile = async (req, res) => {
-  try {
-    const userId = req.params.usrId;
-    const { username, avatar } = req.body;
-
-    if (!userId) {
-      return res.status(400).json({ error: 'User ID not provided in headers.' });
-    }
-
-    const updatedUser = await updateUserProfileData(userId, { username, avatar });
-
-    res.status(200).json({ message: 'Profile updated successfully', user: updatedUser });
-  } catch (error) {
-    console.error('Error updating profile:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
 
 // ✅ Update Avatar + Username (for current user)
 export const updateUserProfileDp = async (req, res) => {

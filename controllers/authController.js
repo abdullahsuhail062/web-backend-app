969a382037de469c3f6c35918a65458584da4bf4
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2025-06-11 11:41:14 +0500

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -42,6 +42,7 @@ export const updateUserProfileDp = async (req, res) => {
     });
 
     const avatarImage = updatedUserDp.avatar
+    console.log(avatarImage)
 
     return res.status(200).json({ message: 'Profile updated successfully', token: newToken, avatar :avatarImage });
   } catch (error) {
